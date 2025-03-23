a8040544c50e18dcb3a3066a0534411ba504346c
Dmytro-Petryk <dimapetrik416@gmail.com>
2025-03-23 21:21:21 +0000

    Deploying to gh-pages from @ Dmytro-Petryk/goit-js-hw-09@d5b6f3016b18ea1edada8b1cfe4ae487c9d4d4ec 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,11 +1,11 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as o}from"./assets/vendor-CgTBfC_f.js";const i="/goit-js-hw-09",n=[{preview:`${i}img/1.png`,original:`${i}img/large1.jpg`,description:"Image description"},{preview:`${i}img/2.png`,original:`${i}img/large2.jpg`,description:"Image description"},{preview:`${i}img/3.png`,original:`${i}img/large3.jpg`,description:"Image description"},{preview:`${i}img/1-1.png`,original:`${i}/img/large1-1.jpg`,description:"Image description"},{preview:`${i}img/2-1.png`,original:`${i}img/large2-1.jpg`,description:"Image description"},{preview:`${i}img/3-1.png`,original:`${i}img/large3-1.jpg`,description:"Image description"},{preview:`${i}img/1-2.png`,original:`${i}img/large1-2.jpg`,description:"Image description"},{preview:`${i}img/2-2.png`,original:`${i}img/large2-2.jpg`,description:"Image description"},{preview:`${i}img/3-2.png`,original:`${i}img/large3-2.jpg`,description:"Image description"}],g=document.querySelector(".gallery");g.innerHTML=n.map(({preview:e,original:r,description:a})=>`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as a}from"./assets/vendor-CgTBfC_f.js";const n=[{preview:"./img/1.png",original:"./img/large1.jpg",description:"Image description"},{preview:"./img/2.png",original:"./img/large2.jpg",description:"Image description"},{preview:"./img/3.png",original:"./img/large3.jpg",description:"Image description"},{preview:"./img/1-1.png",original:"./img/large1-1.jpg",description:"Image description"},{preview:"./img/2-1.png",original:"./img/large2-1.jpg",description:"Image description"},{preview:"./img/3-1.png",original:"./img/large3-1.jpg",description:"Image description"},{preview:"./img/1-2.png",original:"./img/large1-2.jpg",description:"Image description"},{preview:"./img/2-2.png",original:"./img/large2-2.jpg",description:"Image description"},{preview:"./img/3-2.png",original:"./img/large3-2.jpg",description:"Image description"}],e=document.querySelector(".gallery");e.innerHTML=n.map(({preview:i,original:g,description:r})=>`
       <li class="gallery-item">
-        <a class="gallery-link" href="${r}">
+        <a class="gallery-link" href="${g}">
           <img
             class="gallery-image"
-            src="${e}"
-            alt="${a}"
+            src="${i}"
+            alt="${r}"
           />
         </a>
-      </li>`).join("");g.classList.add("gallery-styled");document.addEventListener("DOMContentLoaded",()=>{new o(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,enableKeyboard:!0,overlayOpacity:.8}).on("shown.simplelightbox",()=>{document.querySelector(".sl-overlay").style.backgroundColor="#2E2F42"})});
+      </li>`).join("");e.classList.add("gallery-styled");document.addEventListener("DOMContentLoaded",()=>{new a(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,enableKeyboard:!0,overlayOpacity:.8}).on("shown.simplelightbox",()=>{document.querySelector(".sl-overlay").style.backgroundColor="#2E2F42"})});
 //# sourceMappingURL=1-gallery.js.map
